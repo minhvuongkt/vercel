4300cfa83abf2a66650cfde49a233abbd8c401fb
minhvuongkt <97503105+minhvuongkt@users.noreply.github.com>
2024-03-22 19:32:41 +0700

    T

--- src/re.js
+++ src/re.js
@@ -4,27 +4,32 @@ import { Helmet } from 'react-helmet';
 const Redirector = () => {
     const urlDatabase = {
         "dog": `lovedogs.boonovel.com`,
+        "localhost": `lovedogs.boonovel.com`,
         "imtests": `lovedogs.boonovel.com`,
         "cat": `lovecats.boonovel.com`,
     };
-    let url = "";
+    let url;
     const [imageUrl, setimageUrl] = useState('');
     useEffect(() => {
         const { hostname, pathname } = window.location;
-        const sub = hostname.split('.')[0];
+        let sub = hostname;
+        if (hostname.includes('.')) {
+            sub = hostname.split('.')[0];
+        }
+        //console.log(sub);
         const path = pathname.split('/')[1];
         const path2 = pathname.split('/')[2];
         if (path === 'post' && path2 !== "") {
             for (const key in urlDatabase) {
                 if (sub.includes(key)) {
-                const url = `https://${urlDatabase[key]}/${path2}`;
-                const urlImage = `https://miu2k3.com/catt.jpg`;
-                //window.location.replace(`${url}`);
-                setimageUrl(urlImage);
-            } else {
-                console.log("Invalid URL");
+                    const url = `https://${urlDatabase[key]}/${path2}`;
+                    const urlImage = `https://miu2k3.com/catt.jpg`;
+                    //window.location.replace(`${url}`);
+                    setimageUrl(urlImage);
+                }
             }
-        }
+        } else {
+            console.log("Invalid URL");
         }
     }, [url]);
     return (
