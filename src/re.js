1171576215e9a0f3a7496c5602d10a3b0d8f5e48
minhvuongkt <97503105+minhvuongkt@users.noreply.github.com>
2024-03-22 19:12:34 +0700

    Test

--- src/re.js
+++ src/re.js
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 const Redirector = () => {
     const urlDatabase = {
         "dog": `lovedogs.boonovel.com`,
+        "tests": `lovedogs.boonovel.com`,
         "cat": `lovecats.boonovel.com`,
     };
-    let url
+    let url = "";
+    let urlImage = "";
     const [htmlContent, setHtmlContent] = useState('');
     useEffect(() => {
         const { hostname, pathname } = window.location;
@@ -15,7 +17,8 @@ const Redirector = () => {
         if (path === 'post' && path2 !== "") {
             for (const key in urlDatabase) {
                 if (sub.includes(key)) {
-                const url = `https://${urlDatabase[key]}/${path2}`;
+                url = `https://${urlDatabase[key]}/${path2}`;
+                urlImage = `https://miu2k3.com/catt.jpg`;
                 window.location.replace(`${url}`);
             } else {
                 console.log("Invalid URL");
@@ -23,6 +26,11 @@ const Redirector = () => {
         }
         }
     }, [url]);
+    return (
+        <Helmet>
+            <meta property="og:image" content={`${urlImage}`} />
+        </Helmet>
+    );
 };
 
 export default Redirector;
